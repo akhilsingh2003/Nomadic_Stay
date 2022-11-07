058d330f47e7eba8ddbab78f843e1a8f87e8246b
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2022-11-08 00:09:16 +0530

    completed home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,25 @@
 import "./home.css";
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedPropertis";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 const Home = () => {
   return (
     <div>
     <Navbar/>
     <Header/>
+    <div className="homeContainer">
+    <Featured/>
+    <h1 className="homeTitle">Browse by property type</h1>
+    <PropertyList/>
+    <h1 className="homeTitle">Homes guests love</h1>
+    <FeaturedProperties/>
+    <MailList/>
+    <Footer/>
+    </div>
     </div>
   )
 }
