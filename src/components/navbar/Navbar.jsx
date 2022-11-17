025720062da06f9f1a0002e2b0fdf395e73a1f72
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2022-11-17 23:36:59 +0530

    logo updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,17 @@ function Navbar() {
     <div className="navbar">
         <div className="navContainer">
             <div className="logo">
-            Nomadic Stay
+            {/* Nomadic Stay */}
+            <img src="./gt.png" alt="logo" className="" />  
+            </div>
+            <div className="logo-name">
+              Nomadic Stay 
             </div>
            <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
            </div> 
+ 
         </div>
     </div>
   )
